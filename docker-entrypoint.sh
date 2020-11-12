879510ab226fa026f27f80191e5f669fdab2bf75
ngucandy <anguyen@computer.org>
2020-11-12 15:45:51 -0500

    Update env var name for tunsocks

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-echo -n -e "${PASSWORD}" | exec openconnect --script-tun --script="tunsocks -k 60 -D 0.0.0.0:9000 -H 0.0.0.0:80 -u 22222 $OCPROXY_OPTIONS" -u $USERNAME $URL
+echo -n -e "${PASSWORD}" | exec openconnect --script-tun --script="tunsocks -k 60 -D 0.0.0.0:9000 -H 0.0.0.0:8080 -u 22222 $TUNSOCKS_OPTS" -u $USERNAME $URL
